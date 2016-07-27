87e6cdface45d6c9fa2165b6d697a313c3ba5846
Erik Jung <erik@ot.to>
2016-07-27 12:50:27 -0700

    Updated website.

--- index.js
+++ index.js
@@ -19,14 +19,32 @@ udp_server.on('error', (err) => {
   udp_server.close();
 });
 
+var object = {
+  truckName: "",
+  lat: 0.0,
+  lng: 0.0,
+  active: true
+}
+
 udp_server.on('message', (msg, rinfo) => {
   console.log(`udp_server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  io.emit('geo', parseCoordinates(msg.toString('utf8')));
+  lat_lng = getLatLng(msg.toString('utf8'));
+  var object = {
+    truckName: translateIpToName(rinfo.address),
+    lat: lat_lng.lat,
+    lng: lat_lng.lng,
+    active: true
+  }
+  console.log(object);
+  // io.emit('geo', getLatLng(msg.toString('utf8')));
+  io.emit('geo', object)
 });
 
 udp_server.on('listening', () => {
   var address = udp_server.address();
+  var temp = `208.72.139.82`;
   console.log(`udp_server listening ${address.address}:${address.port}`);
+  console.log(`at&t signal ${temp}:${address.port}`);
 });
 
 udp_server.bind(52323);
@@ -38,13 +56,49 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-var parseCoordinates = function(line) {
-  console.log(line);
-  var array = line.split(",");
-  var coord_pair =  {
-    lat: Number(array[1] / 100) * (array[2] === 'N' ? 1 : -1),
-    lng: Number(array[3] / 100) * (array[4] === 'E' ? 1 : -1)
-  };
-  return coord_pair;
+// This function parses NMEA, RMC values into decimal longitude and latitude coordinates.
+function getLatLng(d) {
+    nmea = d.split(",");
+
+    // LAT: North South
+    coordNS = nmea[3];
+    direction = nmea[4];
+    days = coordNS.substring(0, 2);
+    minutes = coordNS.substring(2, 10);
+    lat = toDD(days,minutes, direction);
+
+    // East West
+    coordEW = nmea[5];
+    direction = nmea[6];
+    days = coordEW.substring(0, 3);
+    minutes = coordEW.substring(3, 11);
+    lon = toDD(days, minutes, direction);
+    // console.log("\n"+lat+"\n"+lon);
+    var coord_pair = {
+      lat: Number(lat),
+      lng: Number(lon)
+    }
+    return coord_pair;
 }
 
+function toDD(degrees, minutes, direction) {
+   out = parseInt(degrees) + (parseFloat(minutes) / 60);
+   if(direction == "S" || direction == "W") {
+      out = out * -1.0;
+   }
+   return out;
+}
+
+function translateIpToName(ip) {
+  var split_addr = ip.split('.');
+  var num_trucks = 5;
+  // Usually it's aaa.bbb.ccc.ddd for ip addresses
+  var ddd = Number(split_addr[3]);
+  var otto_truck = "Otto ";
+  for (var i=1; i<=num_trucks; i++) {
+    if (ddd == i+100) {
+      otto_truck += i.toString();
+      return otto_truck;
+    }
+  }
+}
--- public/app.js
+++ public/app.js
@@ -3,15 +3,15 @@ var socket = io();
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.59746, lng: -87.34688},
-    zoom: 12
+    center: {lat: 37.78, lng: -122.4},
+    zoom: 17
   });
 }
 
 var circle;
 socket.on('geo', function(msg){
 
-  console.log(msg);
+  //console.log(msg);
 
   if (circle) {
     circle.setMap(null);
@@ -23,10 +23,66 @@ socket.on('geo', function(msg){
     fillColor: '#FF0000',
     fillOpacity: 0.35,
     map: map,
-    center: msg,
-    radius: 100
+    center: {
+      lat: 37.78155,
+      lng: -122.3981075
+    },
+    radius: 36
   });
 
-  map.setCenter(msg);
+});
 
+var image_hq;
+var truckMarker =[];
+var truck_names = [];
+var index = 0;
+
+socket.on('geo', function(object) {
+  image_hq = '/trucks/truck_blue_' + object.truckName.slice(5);
+
+  var coordinates = new google.maps.LatLng(object.lat, object.lng);
+
+  if (circle.getBounds().contains(coordinates)) {
+    image_hq = '/trucks/truck_red_' + object.truckName.slice(5);
+  }
+
+  if (truck_names.length > 0) {
+  for (var i=0; i<truck_names.length; i++) {
+    if (truck_names[i] == object.truckName) {
+      truck_names.splice(i,1);
+      --i;
+    }
+  }
+
+  for (var j = 0; j<truckMarker.length; j++) {
+      if(truckMarker[j] == null) {
+        continue;
+      }
+        if((truckMarker[j].title) === object.truckName) {
+          truckMarker[j].setVisible(false);
+          truckMarker[j].setMap(null);
+          truckMarker.splice(j,1);
+          --j;
+          break;
+        }
+      }
+  }
+
+  truck_names.push(object.truckName);
+  marker = new google.maps.Marker({
+    position: {
+      lat:object.lat,
+      lng: object.lng
+    },
+    map: map,
+    icon: image_hq,
+    title: object.truckName
+  });
+  truckMarker[index] = marker;
+  index++;
+
+
+  if(index > 32) {
+    index = 0;
+  }
 });
